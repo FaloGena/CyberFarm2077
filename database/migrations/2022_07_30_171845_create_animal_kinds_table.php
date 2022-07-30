5fd29a1dfeb73ea76e70a7ab12c6b91f3052e1a0
Golygin Ilya <18burg78@gmail.com>
2022-07-31 02:30:11 +0300

    animal kinds parameteres adjusted a little

--- database/migrations/2022_07_30_171845_create_animal_kinds_table.php
+++ database/migrations/2022_07_30_171845_create_animal_kinds_table.php
@@ -24,10 +24,10 @@ class CreateAnimalKindsTable extends Migration
 
         // Sample data
         $animalKinds = [
-            ['name' => 'cat', 'max_size' => 300, 'max_age' => 10, 'growth_factor' => 1.2],
-            ['name' => 'dog', 'max_size' => 500, 'max_age' => 15, 'growth_factor' => 1.4],
-            ['name' => 'bird', 'max_size' => 100, 'max_age' => 5, 'growth_factor' => 1.2],
-            ['name' => 'elephant', 'max_size' => 700, 'max_age' => 40, 'growth_factor' => 1.1],
+            ['name' => 'cat', 'max_size' => 200, 'max_age' => 10, 'growth_factor' => 1.3],
+            ['name' => 'dog', 'max_size' => 300, 'max_age' => 15, 'growth_factor' => 1.2],
+            ['name' => 'bird', 'max_size' => 100, 'max_age' => 5, 'growth_factor' => 1.5],
+            ['name' => 'elephant', 'max_size' => 450, 'max_age' => 30, 'growth_factor' => 1.1],
         ];
 
         \App\Models\AnimalKind::insert($animalKinds);
